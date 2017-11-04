2dea2571f3e4f606771d72c25fd577ea11f715fd
ganesh <ganeshkishan9746@gmail.com>
2017-11-04 14:35:37 +0530

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,8 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  dialogFlow:{
+    angularBot:'6d83fec426d64d5cb162bda8bdc02ae9'
+  }
 };
